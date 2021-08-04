a592e18217cdda89c64aa13bc41c85a3a08d05b1
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-08-04 16:00:51 -0300

    remove price box

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -9,37 +9,41 @@ const onClick = () =>
   gaEvent({ action: 'click', category: 'buy', label: 'pricing box button' })
 
 const PricingBox = ({ landingPagePriceCard }) => {
-  return landingPagePriceCard?.map((item, index) => (
-    <S.Box key={index}>
-      <S.Prices>
-        <S.FullPrice>{item.priceCardLabel}</S.FullPrice>
-        <S.DiscountPrice>{item.priceCardTitle}</S.DiscountPrice>
-      </S.Prices>
-      <S.BenefitsList>
-        {item.priceCardItems.map((subitem) => (
-          <S.BenefitsItem key={subitem.id}>
-            {subitem.conceptItemTitle}
-          </S.BenefitsItem>
-        ))}
-      </S.BenefitsList>
+  return (
+    <>
+      {landingPagePriceCard?.map((item, index) => (
+        <S.Box key={index}>
+          <S.Prices>
+            <S.FullPrice>{item.priceCardLabel}</S.FullPrice>
+            <S.DiscountPrice>{item.priceCardTitle}</S.DiscountPrice>
+          </S.Prices>
+          <S.BenefitsList>
+            {item.priceCardItems.map((subitem) => (
+              <S.BenefitsItem key={subitem.id}>
+                {subitem.conceptItemTitle}
+              </S.BenefitsItem>
+            ))}
+          </S.BenefitsList>
 
-      <Button
-        href={item.priceCardButton.priceCardButtonLink}
-        onClick={onClick}
-        withPrice
-      >
-        <p>{item.priceCardButton.priceCardButtonLabel}</p>
-        <div>
-          <S.ButtonFullPrice>
-            {item.priceCardButton.priceCardButtonPrice}
-          </S.ButtonFullPrice>
-          <S.ButtonDiscountPrice>
-            {item.priceCardButton.priceCardButtonPriceHighlight}
-          </S.ButtonDiscountPrice>
-        </div>
-      </Button>
-    </S.Box>
-  ))
+          <Button
+            href={item.priceCardButton.priceCardButtonLink}
+            onClick={onClick}
+            withPrice
+          >
+            <p>{item.priceCardButton.priceCardButtonLabel}</p>
+            <div>
+              <S.ButtonFullPrice>
+                {item.priceCardButton.priceCardButtonPrice}
+              </S.ButtonFullPrice>
+              <S.ButtonDiscountPrice>
+                {item.priceCardButton.priceCardButtonPriceHighlight}
+              </S.ButtonDiscountPrice>
+            </div>
+          </Button>
+        </S.Box>
+      ))}
+    </>
+  )
 }
 
 export default PricingBox
